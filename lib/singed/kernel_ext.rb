c870ac6b67b00eed4e2ee20793af2f9ec2d5b297
Josh Nichols <josh.nichols@gusto.com>
2023-03-21 17:43:24 -0400

    Add rspec for kernel extension. Update README to reflect label usage

--- lib/singed/kernel_ext.rb
+++ lib/singed/kernel_ext.rb
@@ -1,15 +1,15 @@
 module Kernel
-  def flamegraph(label = nil, open: true, ignore_gc: false, interval: 1000, &block)
+  def flamegraph(label = nil, open: true, ignore_gc: false, interval: 1000, io: $stdout, &block)
     fg = Singed::Flamegraph.new(label: label, ignore_gc: ignore_gc, interval: interval)
     result = fg.record(&block)
     fg.save
 
     if open
       # use npx, so we don't have to add it as a dependency
-      puts "🔥📈 #{'Captured flamegraph, opening with'.colorize(:bold).colorize(:red)}: #{fg.open_command}"
+      io.puts "🔥📈 #{'Captured flamegraph, opening with'.colorize(:bold).colorize(:red)}: #{fg.open_command}"
       fg.open
     else
-      puts "🔥📈 #{'Captured flamegraph to file'.colorize(:bold).colorize(:red)}: #{fg.filename}"
+      io.puts "🔥📈 #{'Captured flamegraph to file'.colorize(:bold).colorize(:red)}: #{fg.filename}"
     end
 
     result

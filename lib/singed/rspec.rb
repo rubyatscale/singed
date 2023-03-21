5159c30a7f908d6a9911e3f6b1dd757b54a856bd
Amir Sharif <budgeneration@gmail.com>
2023-03-21 10:49:42 -0700

    Update rspec.rb

--- lib/singed/rspec.rb
+++ lib/singed/rspec.rb
@@ -1,11 +1,13 @@
 require 'singed'
 
-RSpec.configure do |config|
-  config.around do |example|
-    if example.metadata[:flamegraph]
-      flamegraph { example.run }
-    else
-      example.run
+if defined?(Rspec)
+  RSpec.configure do |config|
+    config.around do |example|
+      if example.metadata[:flamegraph]
+        flamegraph { example.run }
+      else
+        example.run
+      end
     end
   end
 end
